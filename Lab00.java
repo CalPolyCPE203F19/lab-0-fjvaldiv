415734542e5f4c00b36449924b3fa6ea937e258a
fjvaldiv <fjvaldiv@csc302x13.ad.calpoly.edu>
2019-09-23 14:42:50 -0700

    Changed

--- Lab00.java
+++ Lab00.java
@@ -2,6 +2,35 @@ public class Lab00
 {
    public static void main(String[] args)
    {
-   	hello
+      int x = 5;
+      String y = "hello";
+      double z = 9.8;
+        
+      //printing the variables	
+      System.out.println("x: " + x + " y: " + y + " z: " + z);
+
+      //make an array
+      int[] nums = {3, 6, -1, 2};
+      for (int n : nums)
+         System.out.println(n);
+
+      //call a function
+      int numFound = char_count(y, 'l');
+      System.out.println("Found: " + numFound);
+
+      //a counting for loop
+      for (int i = 1; i < 11; i++)
+         System.out.print(i + " ");
+
+      System.out.println();
+   }
+   public static int char_count(String s, char c)
+   {
+      int count = 0;
+      char [] charArray = s.toCharArray();
+      for (char ch : charArray)
+         if (ch == c)
+            count++;
+      return count;
    }
 }
